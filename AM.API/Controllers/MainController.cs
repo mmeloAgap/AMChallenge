9d971786e0324df95761f7e4d06d42020b09602e
mmelo <mmelo@agap2.pt>
2021-01-19 21:37:14 +0000

    Added MainController

---
+++ AM.API/Controllers/MainController.cs
@@ -0,0 +1,28 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace AM.API.Controllers
+{
+    [ApiController]
+    [Route("[controller]/{action}")]
+    public class MainController : ControllerBase
+    {
+        private ILogger<MainController> _logger;
+
+        public MainController(ILogger<MainController> logger)
+        {
+            _logger = logger;
+        }
+
+
+        [HttpGet]
+        public IActionResult Get()
+        {
+            return Ok("Hello World");
+        }
+    }
+}
